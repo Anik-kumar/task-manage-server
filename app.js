dbdc2496a41db65feddbdbad7369d4329d58d9ce
Anik-kumar <anik.kumar.sarker@gmail.com>
2020-09-30 00:22:20 +0600

    added routes for lists | added lists & task model

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('./db/mongoose');
 
+const { List } = require('./db/models/lists');
+const { Task } = require('./db/models/task');
+
+app.use(bodyParser.json());
 
 app.get('/ink', (req, res) => {
   res.send("<h2>The Ink is dry.</h2>");
@@ -11,6 +17,9 @@ app.get('/ink', (req, res) => {
  */
 app.get('/lists', (req, res) => {
   // return array of lists from database
+  List.find({}).then((lists) => {
+    res.send(lists);
+  });
 });
 
 /**
@@ -18,6 +27,13 @@ app.get('/lists', (req, res) => {
  */
 app.post('/lists', (req, res) => {
   // create a new list and return new list to user with id
+  let title = req.body.title;
+  let newList = new List({
+    title: title
+  });
+  newList.save().then((list) => {
+    res.send(list);
+  });
 });
 
 /**
@@ -25,6 +41,18 @@ app.post('/lists', (req, res) => {
  */
 app.patch('/lists/:id', (req, res) => {
   // update list
+  List.findOneAndUpdate(
+    { 
+      _id: req.params.id 
+    },{
+      $set: req.body
+    }).then( () => {
+      res.sendStatus(200);
+      console.log("List is updated");
+    }).catch( err => {
+      console.log("-- List update error --");
+      console.log(err);
+    });
 });
 
 /**
@@ -32,6 +60,23 @@ app.patch('/lists/:id', (req, res) => {
  */
 app.delete('/lists/:id', (req, res) => {
   // delete a list
+  List.findOneAndDelete({
+    _id: req.params.id
+  }).then( (list) => {
+    console.log("List is deleted");
+    res.status(200).send(list);
+  }).catch( err => {
+    console.log("-- List Delete Error --");
+    console.log(err);
+  });
+});
+
+
+/**
+ * returns tasks of list id
+ */
+app.get('/lists/:listId/tasks', (req, res) => {
+
 });
 
 
